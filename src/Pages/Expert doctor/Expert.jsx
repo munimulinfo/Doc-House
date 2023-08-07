77922ee0cebd1dc06dd2c45bf0239aee23c733ae
Shakib AL Nur <shakibalnur3268@gmail.com>
2023-08-07 12:10:50 +0600

    expert added

---
+++ src/Pages/Expert doctor/Expert.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Expert = () => {
+    return (
+        <div className='container'> 
+            <h1 className='text-center font-bold text-4xl'>Our Expert Doctor</h1>
+            <p></p>
+        </div>
+    );
+};
+
+export default Expert;
