35a10abeb0a239ae531b82957583d66baf4951dd
Yeakub1 <mryeakub10@gmail.com>
2023-08-07 12:27:13 +0600

    doc house navbar

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Navbar from '../Shared/Navbar/Navbar';
 
 const Main = () => {
     return (
         <div>
-            Hello Developers
-            <Outlet></Outlet>
+           <Navbar/>
+            <Outlet/>
         </div>
     );
 };
