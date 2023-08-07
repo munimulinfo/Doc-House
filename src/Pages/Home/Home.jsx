44e06250627de87dabab7de6ffff5a088ccd1656
Yeakub1 <mryeakub10@gmail.com>
2023-08-07 13:33:39 +0600

    home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            hlw 
+        <div className='bg-[#07332F]'>
+            <div className="max-w-screen-xl px-5 mx-auto">
+                hello
+           </div>
         </div>
     );
 };
