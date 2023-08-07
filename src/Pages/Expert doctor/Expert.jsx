aa5199954c741f3a9a637989e777388f1157af32
Shakib AL Nur <shakibalnur3268@gmail.com>
2023-08-07 12:36:18 +0600

    added

--- src/Pages/Expert doctor/Expert.jsx
+++ src/Pages/Expert doctor/Expert.jsx
@@ -4,7 +4,7 @@ const Expert = () => {
     return (
         <div className='container'> 
             <h1 className='text-center font-bold text-4xl'>Our Expert Doctor</h1>
-            <p></p>
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inve ntore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
         </div>
     );
 };
